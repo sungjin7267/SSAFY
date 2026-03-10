55b06ef791848ba98dba3515a157aeb76d622c9e
sungjin7267 <sungjin7267@gmail.com>
2026-03-10 17:45:24 +0900

    2026/03/10 SSAFY

---
+++ src/section6_1/Main.java
@@ -0,0 +1,33 @@
+package section6_1;
+import java.util.*;
+
+public class Main {
+    public String solution(String str){
+        String answer="YES";
+        Stack<Character>stack=new Stack<>();
+        for(char c:str.toCharArray()){
+            if(stack.isEmpty()){
+                if(c=='('){
+                    stack.push(c);
+                }else return "NO";
+            }else{
+                if(c=='('){
+                    stack.push(c);
+                }else{
+                    char curr=stack.pop();
+                    if(curr==')'){
+                        return "NO";
+                    }
+                }
+            }
+        }
+        if(!stack.isEmpty()) return "NO";
+        return answer;
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        Main T=new Main();
+        System.out.println(T.solution(str));
+    }
+}
